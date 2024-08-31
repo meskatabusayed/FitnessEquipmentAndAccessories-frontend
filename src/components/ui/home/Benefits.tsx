65cfd21d0c202301d4590856e818d3017aa95388
meskatabusayed <meskatabusayed@gmail.com>
2024-08-31 14:36:59 +0530

    complete our benefits section

--- src/components/ui/home/Benefits.tsx
+++ src/components/ui/home/Benefits.tsx
@@ -3,25 +3,25 @@ const benefitsData = [
       title: "High Quality",
       description: "Our products are made with the highest quality materials.",
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH28LpcMvGBljGtKzH4BbkXDpvz8dLoao3gg&s",
+        "./src/assets/high quality.avif",
     },
     {
       title: "Affordable Prices",
       description: "We offer competitive prices on all our products.",
       image:
-        "https://plus.unsplash.com/premium_photo-1682310152051-5d6f901b19b8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8QWZmb3JkYWJsZSUyMFByaWNlc3xlbnwwfHwwfHx8MA%3D%3D",
+        "./src/assets/Affordable Prices.avif",
     },
     {
       title: "Fast Shipping",
       description: "Enjoy fast and reliable shipping on all orders.",
       image:
-        "https://img.freepik.com/premium-vector/fast-shipping-logo_10250-3101.jpg",
+        "./src/assets/Fast Shipping.jpg",
     },
     {
       title: "Customer Support",
       description: "Our team is here to help you 24/7 with any inquiries.",
       image:
-        "https://cdn.educba.com/academy/wp-content/uploads/2017/02/Customer-Support-1.jpg",
+        "./src/assets/Customer Support.avif",
     },
   ];
   
@@ -38,7 +38,7 @@ const benefitsData = [
                 <img
                   src={benefit.image}
                   alt={benefit.title}
-                  className="w-full h-48 object-cover mb-4 rounded"
+                  className="w-full h-72 object-cover mb-4 rounded"
                 />
                 <h3 className="text-xl font-semibold mb-2">{benefit.title}</h3>
                 <p>{benefit.description}</p>
