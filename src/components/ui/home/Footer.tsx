778a94b674c38026e50a3fd832e8930491b408c1
meskatabusayed <meskatabusayed@gmail.com>
2024-08-31 18:17:31 +0530

    contact information update

--- src/components/ui/home/Footer.tsx
+++ src/components/ui/home/Footer.tsx
@@ -2,24 +2,22 @@ const Footer = () => {
     return (
       <footer className="footer bg-neutral text-neutral-content p-10">
       <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
+        <h6 className="footer-title">contact</h6>
+        <p className="link link-hover">Level-4, 34, Awal Centre, Banani, Dhaka</p>
+        
       </nav>
       <nav>
         <h6 className="footer-title">Company</h6>
         <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
+      
         <a className="link link-hover">Jobs</a>
         <a className="link link-hover">Press kit</a>
       </nav>
       <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
+        <h6 className="footer-title">social</h6>
+        <a href="https://www.facebook.com/programmingHero" className="link link-hover">Fecebook</a>
+        <a className="link link-hover">Linkedin</a>
+        <a className="link link-hover">YouTube</a>
       </nav>
     </footer>
     );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import Banner from "../components/ui/home/Banner";
 import Benefits from "../components/ui/home/Benefits";
 import Categories from "../components/ui/home/Categories";
 import Featured from "../components/ui/home/Featured";
+import Footer from "../components/ui/home/Footer";
 import Gallery from "../components/ui/home/Gallery";
 
 const HomePage = () => {
@@ -12,6 +13,7 @@ const HomePage = () => {
       <Featured />
       <Benefits/>
       <Gallery/>
+      <Footer/>
     </div>
   );
 };
